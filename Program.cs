796e6d01a413ba8db9246e2bb77cf9eb6a28b7cc
Temirulan <mtemirulan@gmail.com>
2017-02-03 11:30:49 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,101 +1,86 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassExample
+namespace FileTest
 {
-    class Person
-    {
-        string name;
-        int age;
-        bool canBuyCigarette;
-
-        public Person (string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-            if (this.age >= 18)
-            {
-                this.canBuyCigarette = true;
-            } else
-            {
-                this.canBuyCigarette = false;
-            }
-        }
-
-        public override string ToString()
-        {
-            return "Name: " + this.name + ". Age: " + this.age + ". canBuyCigarette: " + this.canBuyCigarette;
-        }
-    }
-
-    class Rectangle
-    {
-        int w, h;
-        int area;
-
-        public Rectangle (int w, int h)
-        {
-            this.w = w;
-            this.h = h;
-            this.area = this.w * this.h;
-        }
-
-        public int H
-        {
-            get
-            {
-                return this.h;
-            }
-            set
-            {
-                this.h = value;
-                this.area = this.w * this.h;
-            }
-        }
-        public int W
-        {
-            get
-            {
-                return this.w;
-            }
-            set
-            {
-                this.w = value;
-                this.area = this.w * this.h;
-            }
-        }
-        public int Area
-        {
-            get
-            {
-                return this.area;
-            }
-        }
-
-    }
-
     class Program
     {
+
+        static void print(List<Object> entries, int pos)
+        {
+            Console.SetCursorPosition(0, 0);
+            int cur = 0;
+            foreach (Object obj in entries)
+            {
+                if (cur == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                } else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                cur = cur + 1;
+                if (obj is DirectoryInfo)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(((DirectoryInfo)obj).Name);
+                } else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(((FileInfo)obj).Name);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            string name;
-            int age;
-            Console.Write("Enter name: ");
-            name = Console.ReadLine();
-            Console.Write("Enter age: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Person enteredPerson = new Person(name, age);
-            Console.WriteLine(enteredPerson);
-            Console.ReadKey();
+            string path = @"C:\Users\Admin\Desktop";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
 
-            Rectangle rectangle1 = new Rectangle(3, 4);
-            Console.WriteLine(rectangle1.H);
-            rectangle1.Area = 36;
-            Console.WriteLine(rectangle1.H);
-            Console.ReadKey();
+            List<Object> entries = new List<Object>();
+            foreach (DirectoryInfo directory1 in directories)
+            {
+                entries.Add(directory1);
+            }
+            foreach (FileInfo file in files)
+            {
+                entries.Add(file);
+            }
+
+            int pos = 0;
+            
+            while (true)
+            {
+                print(entries, pos);
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (pos > 0)
+                    {
+                        pos--;
+                    } else
+                    {
+                        pos = entries.Count - 1;
+                    }
+                } else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (pos + 1 < entries.Count)
+                    {
+                        pos++;
+                    } else
+                    {
+                        pos = 0;
+                    }
+                } else
+                {
+                }                
+            }
         }
     }
 }
